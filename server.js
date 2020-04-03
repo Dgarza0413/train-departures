56e4d791c9cc75597b16367a1c6a5dae24439f9f
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-04-02 21:54:01 -0500

    working with moment

---
+++ server.js
@@ -0,0 +1,9 @@
+// use moment to map out time left till arrival on the arrivals page
+
+//use moment to map out every departure ex every 20min from current moment it leaves//
+const express = require('express')
+const fs = require("fs")
+const moment = require("moment")
+const PORT = process.env.PORT || 3000;
+const app = express()
+
